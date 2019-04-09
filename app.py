740099170ae374a439d782959aeacfd1554c71be
Keith Scully <keithscully83@gmail.com>
2019-04-09 21:17:08 +0000

    add seed data, basic root route, base.html template and static CSS & JS files

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = "cookbook"
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
+# Seed data for initial dev purposes prior to DB connection
+data = {
+    'allergens': ['milk', 'eggs', 'soy'],
+    'cuisines': ['american', 'british', 'chinese', 'italian'],
+    'users': ['testuser'],
+    'recipies': [],
+    'ingredients': ['flour', 'eggs', 'butter', 'sugar', 'tomato', 'cheese']
+}
+
+
 @app.route('/')
-def hello():
-    return "Hello World"
-    
+def index():
+    return render_template('base.html')
+
+
+# run application
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
