69fb0ef83305e61c7ba6a812629a720e43c9ade1
Keith Scully <keithscully83@gmail.com>
2019-05-13 19:51:34 +0000

    add error handling routes and templates for 404 and 500 errors

--- app.py
+++ app.py
@@ -302,6 +302,15 @@ def insert_recipe():
     flash('Recipe successfully created', 'success')
     return redirect(url_for('recipelist'))
 
+# Error Handling
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def something_wrong(error):
+    return render_template('500.html'), 500
+
 # run application
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
